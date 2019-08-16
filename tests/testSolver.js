597ef487a1eb5ee7aef1f0af18099191e2ea238e
Emmanuel Guyot <emmanuel.guyot@gmail.com>
2019-08-17 01:39:44 +0200

    Test Ok

--- tests/testSolver.js
+++ tests/testSolver.js
@@ -2,7 +2,7 @@ const startTime = new Date();
 
 const solver = require('../index.js');
 
-// Sanity check
+// Check a basic grid (extracted from Page 44 of ...)
 let gridTest1 = [
     [6, null, null,       null, 7, null,        3, null, null],
     [null, null, null,    3, null, null,        1, 2, null],
@@ -28,6 +28,33 @@ let gridOk1 = [
 let gridRes1 = solver.solver(gridTest1);
 let phrase = `The final grid for ${JSON.stringify(gridTest1)} is ${JSON.stringify(gridRes1)}`;
 console.assert(JSON.stringify(gridOk1) === JSON.stringify(gridRes1), phrase);
+
+// Check an exper grid (extracted from Microsoft Sudoku)
+let gridTest2 = [
+    [null, 3, 9,          2, null, null,        null, null, null],
+    [null, 1, null,       null, null, null,     9, 5, null],
+    [2, null, null,       6, null, null,        null, null, 1],
+    [6, null, null,       3, null, null,        null, null, 4],
+    [null, null, 2,       null, 9, null,        5, null, 3],
+    [null, null, null,    null, null, 2,        null, null, null],
+    [1, null, null,       null, 6, 7,           null, null, 9],
+    [null, 5, 7,          null, null, 4,        null, 2, null],
+    [null, null, 3,       9, null, 1,           7, 8, null],
+];
+let gridOk2 = [
+    [5, 3, 9,       2, 1, 8,        4, 6, 7],
+    [8, 1, 6,       7, 4, 3,        9, 5, 2],
+    [2, 7, 4,       6, 5, 9,        8, 3, 1],
+    [6, 9, 1,       3, 8, 5,        2, 7, 4],
+    [7, 8, 2,       4, 9, 6,        5, 1, 3],
+    [3, 4, 5,       1, 7, 2,        6, 9, 8],
+    [1, 2, 8,       5, 6, 7,        3, 4, 9],
+    [9, 5, 7,       8, 3, 4,        1, 2, 6],
+    [4, 6, 3,       9, 2, 1,        7, 8, 5]
+];
+let gridRes2 = solver.solver(gridTest2);
+phrase = `The final grid for ${JSON.stringify(gridTest2)} is ${JSON.stringify(gridRes2)}`;
+console.assert(JSON.stringify(gridOk2) === JSON.stringify(gridRes2), phrase);
 
 // Time to compute
 const duration = new Date() - startTime;
